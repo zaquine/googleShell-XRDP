96a813aedc23c48e4a4c5e23cc6a1dc7df20b4f6
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-23 16:20:52 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,40 +1,40 @@
-echo "Progress Time: 5 Minute"
-echo "Use stabil connection"
+echo "PROGRESS TIME : 5 MNT"
+echo "USE STABIL CONNECTION"
 echo ""
-echo "==> Setting Ngrok Start"
+
+echo ">> Setting Ngrok Start"
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
-sudo wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok.zip > /dev/null 2>&1
-rm ngrok.zip > /dev/null 2>&1
+wget -O ngrok.sh https://bit.ly/3pbeZd0 > /dev/null 2>&1
+chmod +x ngrok.sh
+./ngrok.sh
 read -p "Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP
-echo "==> Setting Ngrok Success...next"
+echo ">> Setting Ngrok Success"
 echo ""
+
 echo "==> Installing X-Rdp Start"
 sudo apt-get install lxde > /dev/null 2>&1
-sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo apt install -y xrdp > /dev/null 2>&1
-./ngrok tcp 3389 &>/dev/null &
-sudo service xrdp start > /dev/null 2>&1
-./ngrok tcp 3389 &>/dev/null &
-sudo service xrdp start > /dev/null 2>&1
-echo "==> Installing X-Rdp Success...next"
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+echo "==> Installing X-Rdp Success"
 echo ""
+
 echo "==> Installing Firefox Start"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
 sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main"
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get install firefox -y > /dev/null 2>&1
-echo "==> Installing Firefox Success...next"
+echo "==> Installing Firefox Success"
 echo ""
+
 echo "============================================="
-echo "              Your Login Info"
+echo "YOUR XRDP INFO"
 echo "============================================="
 read curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "Username: root"
-echo "Password: That you created on first progress"
+echo "Username: bintang"
+echo "Password: That you created"
 echo "============================================="
 echo ""
-echo"."
+
 echo "NOTE: Don't close this tab to keep your RDP still running"
 sleep 9876543210
