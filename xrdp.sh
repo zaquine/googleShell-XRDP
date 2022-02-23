88e59e8436afa827acd79069a3af1d3ee3443588
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-23 17:39:42 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -13,7 +13,7 @@ echo ""
 
 echo "==> Installing X-Rdp Start"
 sudo apt-get install -y lxde > /dev/null 2>&1
-sudo apt install -y xrdp > /dev/null 2>&1
+sudo apt-get install -y xrdp > /dev/null 2>&1
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 echo "==> Installing X-Rdp Success"
 echo ""
