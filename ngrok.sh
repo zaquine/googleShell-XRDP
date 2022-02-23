81ec2069eae7d716c623c520c5afa00258975163
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-23 22:28:46 +0700

    Delete ngrok.sh

--- ngrok.sh
+++
@@ -1,6 +0,0 @@
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok.zip > /dev/null 2>&1
-read -p "Ngrok Authtoken: " CRP
-./ngrok authtoken $CRP 
-clear
-nohup ./ngrok tcp 3389 &>/dev/null &
