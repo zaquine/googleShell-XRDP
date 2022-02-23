9ff37a7982a110965b62636bb3c2c1141ddb5182
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-23 15:34:49 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -3,7 +3,9 @@ echo "Use stabil connection"
 echo ""
 echo "==> Setting Ngrok Start"
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
-sudo wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1 && unzip ngrok.zip && rm ngrok.zip
+sudo wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+rm ngrok.zip > /dev/null 2>&1
 read -p "Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP
 echo "==> Setting Ngrok Success...next"
