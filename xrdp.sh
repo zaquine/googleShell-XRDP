87d937ae44e16bfa4a38d688c165686667affe38
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-23 13:03:52 +0700

    Create xrdp.sh

---
+++ xrdp.sh
@@ -0,0 +1,54 @@
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/32BvV4p > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "-----------------------"
+echo choose ngrok region
+echo "-----------------------"
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3389 &>/dev/null &
+clear
+echo "-----------------------"
+echo "Wait 5 minute for instalation"
+echo "-----------------------"
+sudo apt-get install lxde -y
+clear
+echo "-----------------------"
+echo "Install Firefox"
+echo "-----------------------"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
+sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main"
+echo "-----------------------"
+echo "Update"
+echo "-----------------------"
+sudo apt-get update > /dev/null 2>&1
+echo "-----------------------"
+echo "Installing"
+echo "-----------------------"
+sudo apt-get install firefox -y > /dev/null 2>&1
+echo "-----------------------"
+echo "Install xrdp"
+echo "-----------------------"
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+clear
+echo "-----------------------"
+echo "xrdp instalation success"
+echo "Username : root"
+echo "Username : that you made yourself"
+echo "-----------------------"
+echo "RDP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "-----------------------"
+echo "Don't close this tab to keep RDP running"
+echo "-----------------------"
+sleep 9876543210
