a55ee9b07c917ea79a25dbfcfb47a6ba64e93394
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-23 14:22:18 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,54 +1,37 @@
-rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/32BvV4p > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
-clear
-echo "-----------------------"
-echo choose ngrok region
-echo "-----------------------"
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3389 &>/dev/null &
-clear
-echo "-----------------------"
-echo "Wait 5 minute for instalation"
-echo "-----------------------"
-sudo apt-get install lxde -y
-clear
-echo "-----------------------"
-echo "Install Firefox"
-echo "-----------------------"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
-sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main"
-echo "-----------------------"
-echo "Update"
-echo "-----------------------"
-sudo apt-get update > /dev/null 2>&1
-echo "-----------------------"
-echo "Installing"
-echo "-----------------------"
-sudo apt-get install firefox -y > /dev/null 2>&1
-echo "-----------------------"
-echo "Install xrdp"
-echo "-----------------------"
+echo "Progress Time: 5 Minute"
+echo "Use stabil connection"
+sleep 5
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "Setting Ngrok Start"
+sudo wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1 && unzip ngrok.zip && rm ngrok.zip
+read -p "Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP
+echo "Setting Ngrok Success...next"
+
+sleep 5
+echo "Installing X-Rdp Start"
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+./ngrok tcp 3389 &>/dev/null &
 sudo service xrdp start > /dev/null 2>&1
-clear
-echo "-----------------------"
-echo "xrdp instalation success"
-echo "Username : root"
-echo "Username : that you made yourself"
-echo "-----------------------"
-echo "RDP Address:"
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "-----------------------"
-echo "Don't close this tab to keep RDP running"
-echo "-----------------------"
+echo "Installing X-Rdp Success...next"
+
+sleep 5
+echo "Installing Firefox Start"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
+sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main"
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install firefox -y > /dev/null 2>&1
+echo "Installing Firefox Success...next"
+
+sleep 5
+echo "============================================="
+echo "              Your Login Info"
+echo "============================================="
+read curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Username: root"
+echo "Password: That you created on first progress"
+echo "============================================="
+sleep 2
+echo "NOTE: Don't close this tab to keep your RDP still running"
 sleep 9876543210
