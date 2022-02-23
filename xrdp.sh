8d1f7a015a3b8a238d72f73a94674c723e6f0126
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-23 19:37:12 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,41 +1,51 @@
 echo "PROGRESS TIME : 5 MNT"
 echo "USE STABIL CONNECTION"
 echo ""
-
-echo "==> Setting Ngrok Start"
 echo ""
 rm -fr google-xrdp.sh  ngrok  ngrok.zip
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-read -p "Ngrok Authtoken: " CRP
+read -p "> Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region ap 3389 &>/dev/null &
+echo "====================="
+echo "SETTING NGROK DONE"
+echo "====================="
 echo ""
-echo "==> Setting Ngrok Success"
 echo ""
-
-echo "==> Installing X-Rdp Start"
-echo ""
-echo "wait until progress finish"
 sudo apt install lxde > /dev/null 2>&1
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+echo "======================"
+echo "INSTALLING X-RDP DONE"
+echo "======================"
 echo ""
-echo "==> Installing X-Rdp Success"
 echo ""
-
+echo "Installing Firefox Start"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
+sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main"
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install firefox -y > /dev/null 2>&1
+echo "======================"
+echo "INSTALLING X-RDP DONE"
+echo "======================"
+echo ""
+echo ""
+echo "> WAIT FOR COMPLETE PROGRESS <"
 sudo apt install apt-transport-https curl > /dev/null 2>&1
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
 sudo apt update > /dev/null 2>&1
 sudo apt install brave-browser > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
-echo "==========================="
-echo "YOUR XRDP INFO"
-echo "==========================="
+echo ""
+echo ""
+echo "======================"
+echo "YOUR X-RDP INFO"
+echo "======================"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Username: bintang"
-echo "Password: That you created"
+echo "Password: that you created"
 echo "==========================="
 echo ""
 
